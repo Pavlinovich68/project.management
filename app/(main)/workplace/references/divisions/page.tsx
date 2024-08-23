f2e66ddfe978242a1372e3b4d40aed7efe73fe37
Pavlov Sergey <sergey@pavlinovich.ru>
2024-08-23 07:45:08 +0500

    Поправил CRUD по подразделениям

--- app/(main)/workplace/references/divisions/page.tsx
+++ app/(main)/workplace/references/divisions/page.tsx
@@ -6,7 +6,7 @@ import {Button} from 'primereact/button';
 import {InputText} from 'primereact/inputtext';
 import {ICardRef} from '../../../../../models/ICardRef'
 import {FormikErrors, useFormik} from "formik";
-import {Division} from "../../../../../models/IDivision";
+import {IDivision} from "../../../../../models/IDivision";
 import RecordState from "@/models/enums/record-state";
 import {Toast} from "primereact/toast";
 import {ConfirmDialog} from "primereact/confirmdialog";
@@ -14,14 +14,11 @@ import ItrCard from "@/components/ItrCard";
 import {classNames} from "primereact/utils";
 import { ConfirmPopup } from 'primereact/confirmpopup';
 import CrudHelper from "@/services/crud.helper.js"
-import AttachService from "@/services/attachment.service"
 import CRUD from "@/models/enums/crud-type";
-import { TabView, TabPanel } from 'primereact/tabview';
-import { FileUpload, FileUploadHeaderTemplateOptions, ItemTemplateOptions } from 'primereact/fileupload';
 
 const Divisions = () => {
    const controllerName = "division";
-   const emptyDivision: Division = {};
+   const emptyDivision: IDivision = {};
    const toast = useRef<Toast>(null);
    const editor = useRef<ICardRef>(null);
    const [divisions, setDivisions] = useState([]);
@@ -31,14 +28,7 @@ const Divisions = () => {
    const [isLoading, setIsLoading] = useState<boolean>(false);
    const [visibleConfirm, setVisibleConfirm] = useState(false);
    const [globalFilter, setGlobalFilter] = useState<string>('');
-   const [deletedDivision, setDeletedDivision] = useState<Division>(emptyDivision);
-   const fileUploadRef = useRef<FileUpload>(null);
-   const chooseOptions = { icon: 'pi pi-fw pi-images', iconOnly: true, className: 'custom-choose-btn p-button-rounded p-button-outlined' };
-   const uploadOptions = { icon: 'pi pi-fw pi-cloud-upload', iconOnly: true, className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined' };
-   const cancelOptions = { icon: 'pi pi-fw pi-times', iconOnly: true, className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined' };
-   const [imageSrc, setImageSrc] = useState('');
-   const [attachChanged, setAttachChanged] = useState<boolean>(false);
-   const [attachmentId, setAttachmentId] = useState<number | undefined | null>(null);
+   const [deletedDivision, setDeletedDivision] = useState<IDivision>(emptyDivision);;
 
    useEffect(() => {
       CrudHelper.crud(controllerName, CRUD.read, {}).then((result)=>{
@@ -47,15 +37,12 @@ const Divisions = () => {
    }, []);
 
    //#region Card
-   const division = useFormik<Division>({
+   const division = useFormik<IDivision>({
       initialValues: emptyDivision,
       validate: (data) => {
-         let errors: FormikErrors<Division> = {};
+         let errors: FormikErrors<IDivision> = {};
          if (!data.name){
             errors.name = "Наименование подразделения должно быть заполнено!";
-         }
-         if (!data.short_name){
-            errors.short_name = "Короткое наименование подразделения должно быть заполнено!";
          }
          return errors;
       },
@@ -64,91 +51,24 @@ const Divisions = () => {
       }
    });
 
-   const headerTemplate = (options: FileUploadHeaderTemplateOptions) => {
-      const { className, chooseButton, cancelButton } = options;
-
-      return (
-         <div className={className} style={{ backgroundColor: 'transparent', display: 'flex', alignItems: 'center' }}>
-            {chooseButton}
-            {cancelButton}
-         </div>
-      );
-   };
-
-   const itemTemplate = (inFile: object, props: ItemTemplateOptions) => {
-      const file = inFile as File;
-      //@ts-ignore
-      const objectURL = file.objectURL;
-      return (
-         <div className="flex align-items-center flex-wrap">
-            <div className="flex align-items-center" style={{ width: '40%' }}>
-               <img alt={file.name} role="presentation" src={objectURL} width={100} />
-            </div>
-         </div>
-      );
-   };
-
-   const loadedTemplate = () => {
-      return (
-         <div className="flex align-items-center flex-wrap">
-            <div className="flex align-items-center" style={{ width: '40%' }}>
-               <img alt={"loadedFromDataBaseImage"} role="presentation" src={imageSrc} width={100} />
-            </div>
-         </div>
-      );
-   };
-
-   const emptyTemplate = () => {
-      return (
-         <div className="flex align-items-center flex-column">
-            <i className="pi pi-image mt-3 p-5" style={{ fontSize: '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
-            <span style={{ fontSize: '1.2em', color: 'var(--text-color-secondary)' }} className="my-5">
-               Перетащите сюда изображение
-            </span>
-         </div>
-      );
-   };
-
    const card = (
       <div className="card p-fluid">
-         <TabView>
-            <TabPanel header="Основные данные">
-               <div className="p-fluid formgrid grid">
-                  <div className="field col-12">
-                     <label htmlFor="name">Наименование подразделения</label>
-                     <InputText id="name"  placeholder="Наименование"
-                                          className={classNames({"p-invalid": submitted && !division.values.name})}
-                                          value={division.values.name}
-                                          onChange={(e) => division.setFieldValue('name', e.target.value)} required autoFocus type="text"/>
-                  </div>
-                  <div className="field col-12 md:col-6">
-                     <label htmlFor="short_name">Короткое наименование</label>
-                     <InputText id="short_name"  placeholder="Короткое наименование"
-                                       className={classNames({"p-invalid": submitted && !division.values.short_name})}
-                                       value={division.values.short_name}
-                                       onChange={(e) => division.setFieldValue('short_name', e.target.value)} required autoFocus type="text"/>
-                  </div>
-                  <div className="field col-12 md:col-6">
-                     <label htmlFor="contacts">Контактная информация</label>
-                     <InputText id="contacts" placeholder="Контактная информация"
-                                       value={division.values.contacts}
-                                       onChange={(e) => division.setFieldValue('contacts', e.target.value)} required autoFocus type="text"/>
-                  </div>
-               </div>
-            </TabPanel>
-            <TabPanel header="Логотип">
-            <FileUpload ref={fileUploadRef} accept="image/*" maxFileSize={1000000}
-                  headerTemplate={headerTemplate} itemTemplate={itemTemplate} emptyTemplate={imageSrc === '' ? emptyTemplate : loadedTemplate}
-                  chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions} onSelect={(e) => setAttachChanged(true) } onRemove={(e) => setAttachChanged(true) }/>
-            </TabPanel>
-         </TabView>
+         <div className="p-fluid formgrid grid">
+            <div className="field col-12">
+               <label htmlFor="name">Наименование подразделения</label>
+               <InputText id="name"  placeholder="Наименование"
+                                    className={classNames({"p-invalid": submitted && !division.values.name})}
+                                    value={division.values.name}
+                                    onChange={(e) => division.setFieldValue('name', e.target.value)} required autoFocus type="text"/>
+            </div>
+         </div>
       </div>
    )
 
    //#endregion
 
    //#region CRUD
-   const createDivision = (data: Division | null) => {
+   const createDivision = (data: IDivision | null) => {
       setCardHeader('Создание нового подразделения');
       emptyDivision.parent_id = data?.id;
       division.setValues(emptyDivision);
@@ -159,11 +79,9 @@ const Divisions = () => {
       }
    }
 
-   const editDivision = async (data: Division) => {
+   const editDivision = async (data: IDivision) => {
       setCardHeader('Редактирование подразделения');
       division.setValues(data);
-      const attach = await AttachService.read(data.attachment_id);
-      setImageSrc(attach);
       setRecordState(RecordState.edit);
       setSubmitted(false);
       if (editor.current) {
@@ -209,28 +127,14 @@ const Divisions = () => {
       try {
          setIsLoading(true);
 
-         if (attachChanged) {
-            const attach = fileUploadRef.current?.getFiles()[0];
-            if (attach) {
-               const attachResult = await AttachService.save(attach, attachmentId);
-               division.values.attachment_id = attachResult.data.id;
-            }
-         }
-
          const res = recordState === RecordState.new ?
             await CrudHelper.crud(controllerName, CRUD.create, {
                name: division.values.name,
-               short_name: division.values.short_name,
-               contacts: division.values.contacts,
                parent_id: division.values.parent_id,
-               attachment_id: division.values.attachment_id
             }) :
             await CrudHelper.crud(controllerName, CRUD.update, {
                id: division.values.id,
-               name: division.values.name,
-               short_name: division.values.short_name,
-               contacts: division.values.contacts,
-               attachment_id: division.values.attachment_id
+               name: division.values.name
             });
 
          if (res.status === 'error'){
