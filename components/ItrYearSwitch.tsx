4d1e06afb0c247def845dc40b922c5f9d287f200
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2024-09-20 22:59:24 +0500

    Переделал переключатель года в графике отпусков

--- components/ItrYearSwitch.tsx
+++ components/ItrYearSwitch.tsx
@@ -1,21 +1,19 @@
 'use client'
 import { Button } from "primereact/button";
+import { Calendar } from "primereact/calendar";
 import React, {useRef, useState, useEffect} from "react";
 
 const ItrYearSwitsh = ({year, onChange}: {year: number, onChange: any}) => {
-   const [currentYear, setCurrentYear] = useState<number>(year)
-
-   const buttonClick = (cnt : number) => {
-      const _year = currentYear + cnt;
-      setCurrentYear(_year);
-      onChange(_year);
-   }
+   const [date, setDate] = useState<Date>(new Date(year, 0, 1))
 
    return (
       <div className="p-inputgroup calendar-switch flex justify-content-center flex-wrap">
-         <Button icon="pi pi-arrow-left" onClick={() => buttonClick(-1)}/>
-         <div className="center-area">{currentYear}</div>
-         <Button icon="pi pi-arrow-right" onClick={() => buttonClick(1)}/>
+         <div className="flex align-items-center justify-content-center w-2">
+         <Calendar className="prod-calendar-switch" value={date} onChange={(e) => {
+               setDate(e.value as Date);
+               onChange(e.value?.getFullYear());
+            }} view="year" dateFormat="yy 'год'" />
+         </div>
       </div>   
    );
 };
