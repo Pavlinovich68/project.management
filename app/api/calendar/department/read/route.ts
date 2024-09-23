c653d0b3eafec5f0e86fa02158365ffcdb79c9f0
Pavlov Sergey <sergey@pavlinovich.ru>
2024-09-23 15:56:19 +0500

    !!!

--- app/api/calendar/department/read/route.ts
+++ app/api/calendar/department/read/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (request: NextRequest) => {
    try {
       const { division_id, year, month } = await request.json();
-
+// Календарь
       const _calendar = await prisma.dept_calendar.findFirst({
          where: {
             year: year,
@@ -15,10 +15,10 @@ export const POST = async (request: NextRequest) => {
 
       if (!_calendar)
          return await NextResponse.json({status: 'error', data: "Календарь не обнаружен!" });
-
+// Колонки
       const dayCount = new Date(year, month, 0).getDate();
       const dayArray = Array.from(Array(dayCount+1).keys()).filter(i => i>0);
-
+// Выходная модель
       const result: ICalendar = {
          year: year,
          month: month,
@@ -27,115 +27,123 @@ export const POST = async (request: NextRequest) => {
          footer: undefined
       }
       
-      const _rows = await prisma.dept_calendar_row.findMany({
+      const rows = await prisma.rate.findMany({
          where: {
-            calendar_id: _calendar.id
+            division_id: division_id
          },
          orderBy: {
-            rate: {
-               no: 'asc'
-            }
+            no: 'asc'
          }
-      });
+      });      
+      // const _rows = await prisma.dept_calendar_row.findMany({
+      //    where: {
+      //       calendar_id: _calendar.id
+      //    },
+      //    orderBy: {
+      //       rate: {
+      //          no: 'asc'
+      //       }
+      //    }
+      // });
 
-      for (const _row of _rows) {
-         const _staff = await prisma.staff.findFirst({ 
-            where: {
-               rate_id: _row.rate_id,
-               begin_date: {
-                  lte: new Date()
-               },
-               OR: [
-                  {
-                     end_date: {
-                        gte: new Date()
-                     }
-                  },
-                  {
-                     end_date: null
-                  }
-               ]
-            } 
-         });
-         let _rowHeader = 'Вакансия';
-         if (_staff) {
-            const _employee = await prisma.employee.findFirst({ where: {id: _staff.employee_id } });
-            if (_employee) {
-               _rowHeader = _employee.surname + ' ' + _employee.name?.charAt(0) + '.' + _employee.pathname?.charAt(0) + '.'
-            } else {
-               _rowHeader = 'Вакансия';
-            }
-         }
+      // for (const _row of _rows) {
+      //    const _staff = await prisma.staff.findFirst({ 
+      //       where: {
+      //          rate_id: _row.rate_id,
+      //          begin_date: {
+      //             lte: new Date()
+      //          },
+      //          OR: [
+      //             {
+      //                end_date: {
+      //                   gte: new Date()
+      //                }
+      //             },
+      //             {
+      //                end_date: null
+      //             }
+      //          ]
+      //       } 
+      //    });
+      //    let _rowHeader = 'Вакансия';
+      //    if (_staff) {
+      //       const _employee = await prisma.employee.findFirst({ where: {id: _staff.employee_id } });
+      //       if (_employee) {
+      //          _rowHeader = _employee.surname + ' ' + _employee.name?.charAt(0) + '.' + _employee.pathname?.charAt(0) + '.'
+      //       } else {
+      //          _rowHeader = 'Вакансия';
+      //       }
+      //    }
 
-         const _cells = await prisma.dept_calendar_cell.findMany({
-            where: {
-               row_id: _row.id,
-               month: month
-            },
-            orderBy: {
-               day: 'asc'
-            }
-         });
+      //    const _cells = await prisma.dept_calendar_cell.findMany({
+      //       where: {
+      //          row_id: _row.id,
+      //          month: month
+      //       },
+      //       orderBy: {
+      //          day: 'asc'
+      //       }
+      //    });
 
-         const _sum = _cells.map(i => i.hours).reduce((part, a) => part + a, 0);
-         const cells:ICalendarCell[] = _cells.map(i => {
-            return {
-               id: i.id,
-               day: i.day,
-               type: i.type,
-               hours: i.hours,
-            }
-         })
+      //    const _sum = _cells.map(i => i.hours).reduce((part, a) => part + a, 0);
+      //    const cells:ICalendarCell[] = _cells.map(i => {
+      //       return {
+      //          id: i.id,
+      //          day: i.day,
+      //          type: i.type,
+      //          hours: i.hours,
+      //       }
+      //    })
          
-         const _total = await prisma.dept_calendar_cell.aggregate({
-            where: {
-               row_id: _row.id,
-               month: {
-                  lte: month
-               }
-            },
-            _sum: {
-               hours: true
-            }
-         })
+      //    const _total = await prisma.dept_calendar_cell.aggregate({
+      //       where: {
+      //          row_id: _row.id,
+      //          month: {
+      //             lte: month
+      //          }
+      //       },
+      //       _sum: {
+      //          hours: true
+      //       }
+      //    })
          
-         const row: ICalendarRow = {
-            name: _rowHeader,
-            cells: cells,
-            hours: _sum,
-            total: _total._sum.hours??0
-         }
+      //    const row: ICalendarRow = {
+      //       name: _rowHeader,
+      //       cells: cells,
+      //       hours: _sum,
+      //       total: _total._sum.hours??0
+      //    }
 
-         result.rows?.push(row);
-      }
+      //    result.rows?.push(row);
+      // }
       
-      const _footer: ICalendarFooter = {name: 'Итого', hours: [], sum: undefined, total: 0};
+      // const _footer: ICalendarFooter = {name: 'Итого', hours: [], sum: undefined, total: 0};
 
-      for (const _day of dayArray) {
-         const _sum = await prisma.dept_calendar_cell.aggregate({
-            where: {
-               day: _day,
-               month: month,
-               row: {
-                  calendar: {
-                     division_id: division_id
-                  }
-               }
-            },
-            _sum: {
-               hours: true
-            }
-         });
+      // for (const _day of dayArray) {
+      //    const _sum = await prisma.dept_calendar_cell.aggregate({
+      //       where: {
+      //          day: _day,
+      //          month: month,
+      //          row: {
+      //             calendar: {
+      //                division_id: division_id
+      //             }
+      //          }
+      //       },
+      //       _sum: {
+      //          hours: true
+      //       }
+      //    });
 
-         _footer.hours?.push(_sum._sum.hours??0);
-      }
+      //    _footer.hours?.push(_sum._sum.hours??0);
+      // }
 
-      _footer.sum = _footer.hours?.reduce((item, a) => item + a, 0);
-      _footer.total = 0;
-      for (const _row of result.rows??[]) {
-         _footer.total += _row.total??0;
-      }
-      result.footer = _footer;
+      // _footer.sum = _footer.hours?.reduce((item, a) => item + a, 0);
+      // _footer.total = 0;
+      // for (const _row of result.rows??[]) {
+      //    _footer.total += _row.total??0;
+      // }
+      // result.footer = _footer;
       
       return await NextResponse.json({status: 'success', data: result});
    } catch (error) {
