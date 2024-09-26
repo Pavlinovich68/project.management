6af9c8584b7a894736abe89e5339147cc5f1c510
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2024-09-27 04:39:02 +0500

    !!!

--- components/calendar/ItrCalendarRow.tsx
+++ components/calendar/ItrCalendarRow.tsx
@@ -6,34 +6,6 @@ import { ICalendar, ICalendarRow } from "@/models/ICalendar";
 import ItrCalendarCell from "./ItrCalendarCell";
 
 const ItrCalendarRow = ({row, writeMode, dayType}:{row:ICalendarRow, writeMode: boolean, dayType: number | undefined}) => {
-   const [start, setStart] = useState<number>(0);
-   const [end, setEnd] = useState<number>(0);
-   const [selecting, setSelecting] = useState<boolean>(false);
-   const [startUpdate, setStartUpdate] = useState<boolean>(false)
-
-   const beginSelection = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, i: number) => {
-      if (!writeMode) return;
-      setStartUpdate(true);
-      setSelecting(true);
-      setStart(i);
-      updateSelection(e, i);
-   };
-   const endSelection = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, i = end) => {      
-      if (!writeMode || !startUpdate) return;
-      const el = e.target as HTMLDivElement;
-      console.log(`end ${el.getAttribute('data-base-type')}`);
-      setStartUpdate(false)
-      setSelecting(false);
-      updateSelection(e, i);
-   };
-
-   const updateSelection = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, i: number) => {      
-      if (!writeMode || !startUpdate) return;
-      if (selecting) {
-         setEnd(i);
-      }
-   };
-
    return (
       <React.Fragment>
          <div key="row" className={classNames("flex justify-content-center", styles.calendarRow)}>
