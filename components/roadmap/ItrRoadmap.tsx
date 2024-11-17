c629f6e1f345cfe4848bc6ea787a4cafef2b1ccb
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2024-11-17 08:28:21 +0500

    Карточка

--- components/roadmap/ItrRoadmap.tsx
+++ components/roadmap/ItrRoadmap.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { classNames } from "primereact/utils";
-import React, {useRef, useState, useEffect, Ref, forwardRef, useImperativeHandle} from "react";
+import React, {useRef, useState, useEffect} from "react";
 import ItrRoadmapRow from "./ItrRoadmapRow";
 import { IRoadmapItem, IRoadmapItemCRUD } from "@/models/IRoadmapItem";
 import styles from "@/app/(main)/workplace/department/roadmap/styles.module.scss"
@@ -14,6 +14,14 @@ import crudHelper from "@/services/crud.helper";
 import RecordState from "@/models/enums/record-state";
 import CRUD from "@/models/enums/crud-type";
 import { Toast } from "primereact/toast";
+import {confirmDialog} from "primereact/confirmdialog";
+import { InputText } from "primereact/inputtext";
+import { TreeSelect, TreeSelectChangeEvent } from "primereact/treeselect";
+import { TreeNode } from "primereact/treenode";
+import { IProjectNode } from "@/models/IProjectNode";
+import { IBaseEntity } from "@/models/IBaseEntity";
+import { Calendar } from "primereact/calendar";
+import { InputNumber } from "primereact/inputnumber";
 
 const Roadmap = ({year, division_id}:{year: number, division_id: number}) => {
    const model: IRoadmapItemCRUD = {
@@ -36,11 +44,19 @@ const Roadmap = ({year, division_id}:{year: number, division_id: number}) => {
    const [recordState, setRecordState] = useState<RecordState>(RecordState.ready);
    const toast = useRef<Toast>(null);
    const editor = useRef<ICardRef>(null);
+   const [selectedNodeKey, setSelectedNodeKey] = useState<string | null>(null);
+   const [nodes, setNodes] = useState<IProjectNode[]>([]);
+   const [selectedProjectId, setSelectedProjectId] = useState<number | undefined>(undefined);
 
    useEffect(() => {
       getRoadmapData(year, division_id);
       getPointer(year);
    }, [year])   
+
+   const getProjectTree = async () => {
+      const response = await crudHelper.crud('project', CRUD.read, {});
+      setNodes(response.data);
+   }
    
    const getPointer = (year: number) => {
       const length = new Date(year, 2, 0).getDate() === 28 ? 365 : 366;
@@ -89,17 +105,67 @@ const row = useFormik<IRoadmapItemCRUD>({
 });
 
 const card = (
-   <div className="card p-fluid">
+   <div className={classNames("card p-fluid", styles.card)}>
       <i className="pi pi-spin pi-spinner" style={{ fontSize: '10rem', color: '#326fd1', zIndex: "1000", position: "absolute", left: "calc(50% - 5rem)", top: "calc(50% - 5rem)", display: `${isLoading ? 'block' : 'none'}`}} hidden={!isLoading}></i>
-      <div className="p-fluid formgrid grid">         
+      <div className="p-fluid formgrid grid">
+         {recordState === RecordState.ready ? 
+            <React.Fragment>
+               <h5>Просмотр</h5>
+            </React.Fragment> : 
+            <React.Fragment>
+               <div className="field col-12">
+                  <label htmlFor="name">Проект</label>
+                  <TreeSelect 
+                     value={selectedNodeKey}
+                     options={nodes}
+                     onChange={(e : TreeSelectChangeEvent) => {
+                        //@ts-ignore
+                        setSelectedNodeKey(e.value)
+                        //@ts-ignore
+                        row.setFieldValue('project_id', parseInt(e.value));
+                     }}
+                  />
+               </div>
+               <div className="field col-12">
+                  <label htmlFor="name">Наименование работ</label>
+                  <InputText id="name"  placeholder="Наименование работ"
+                                       className={classNames({"p-invalid": submitted && !row.values.comment})}
+                                       value={row.values.comment}
+                                       onChange={(e) => row.setFieldValue('comment', e.target.value)} required autoFocus type="text"/>
+               </div>               
+               <div className="field col-12 md:col-6">
+                  <label htmlFor="begin_date">Дата начала</label>
+                  <Calendar id="begin_date" className={classNames({"p-invalid": submitted && !row.values.start_date})} value={new Date(row.values.start_date as Date)} onChange={(e) => row.setFieldValue('start_date', e.target.value)} dateFormat="dd MM yy" locale="ru" showIcon required  showButtonBar tooltip="Дата начала"/>
+               </div>
+               <div className="field col-12 md:col-6">
+                  <label htmlFor="end_date">Дата окончания</label>
+                  <Calendar id="end_date" value={row.values.end_date !== null ? new Date(row.values.end_date as Date) : null} onChange={(e) => row.setFieldValue('end_date', e.target.value)} dateFormat="dd MM yy" locale="ru" showIcon required  showButtonBar tooltip="Дата окончания"/>
+               </div>
+               <div className="field col-6">
+                  <label htmlFor="hours">Плановое количество часов</label>
+                  <InputNumber id="hours"  placeholder="Плановое количество часов"
+                                       className={classNames({"p-invalid": submitted && !row.values.hours})}
+                                       value={row.values.hours}
+                                       onValueChange={(e) => row.setFieldValue('hours', e.value)} required autoFocus/>
+               </div>
+               <div className="field col-6">
+                  <label htmlFor="developer_qnty">Плановая численность</label>
+                  <InputNumber id="developer_qnty"  placeholder="Плановая численность привлеченных разработчиков"
+                                       className={classNames({"p-invalid": submitted && !row.values.developer_qnty})}
+                                       value={row.values.developer_qnty}
+                                       onValueChange={(e) => row.setFieldValue('developer_qnty', e.value)} required autoFocus/>
+               </div>
+            </React.Fragment>}
       </div>
    </div>
 )
 //#endregion //!SECTION CARD
 //#region //!SECTION CRUD
-   const createMethod = () => {
-      console.log('OK');
+   const createMethod = async () => {
+      console.log('Create');
       setCardHeader('Создание нового элемента плана');
+      await getProjectTree();
+      setSelectedNodeKey(null);
       row.setValues(model);
       setRecordState(RecordState.new);
       setSubmitted(false);
@@ -108,16 +174,42 @@ const card = (
       }
    }
 
-   const updateMethod = (item: IRoadmapItemCRUD) => {
+   const updateMethod = async (item: IRoadmapItemCRUD) => {
       console.log('Update: ', item);
+      setCardHeader('Редактирование элемента плана');
+      await getProjectTree();
+      row.setValues(item);
+      setSelectedNodeKey(item.project_id?.toString()??null);
+      setRecordState(RecordState.edit);
+      setSubmitted(false);
+      if (editor.current) {
+         editor.current.visible(true);
+      }
    }
 
-   const deleteMethod = (item: IRoadmapItemCRUD) => {
-      console.log('Drop: ', item);
+   const deleteMethod = (item: IRoadmapItemCRUD) => {      
+      confirmDialog({
+         message: `Вы уверены что хотите удалить запись?`,
+         header: 'Удаление записи',
+         icon: 'pi pi-exclamation-triangle text-red-500',
+         acceptLabel: 'Да',
+         rejectLabel: 'Нет',
+         showHeader: true,
+         accept: () => {
+            console.log('Drop: ', item);
+         }
+      });
    }
 
    const viewMethod = (item: IRoadmapItemCRUD) => {
       console.log('View: ', item);
+      setCardHeader('Просмотр элемента плана');
+      row.setValues(item);
+      setRecordState(RecordState.ready);
+      setSubmitted(false);
+      if (editor.current) {
+         editor.current.visible(true);
+      }
    }
 
    const saveMethod = async () => {
@@ -172,7 +264,7 @@ const card = (
 //#endregion //!SECTION CRUD
 
    const button = (<Button icon="pi pi-plus" className="mr-2" onClick={() => createMethod()}/>);
-
+   
    return (
       <div className="card" style={{position: "relative"}}>
          <Toolbar start={button} style={{marginTop: "1rem"}}/>
