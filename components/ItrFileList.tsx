d968dfaedec7401191d8c628709e378ce78b6516
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2025-03-10 04:05:54 +0500

    roadmap_item documents

--- components/ItrFileList.tsx
+++ components/ItrFileList.tsx
@@ -3,6 +3,18 @@ import React, {useRef, useState, useEffect} from "react";
 import { DataScroller } from 'primereact/datascroller';
 
 const ItrFileList = ({bucketName}:{bucketName: string}) => {
+
+   const readFiles = async () => {
+      const res = await fetch(`/api/minio/fileList`, {
+         method: "GET",
+         headers: {
+            "Content-Type": "application/json",
+         }
+      });
+      const data = await res.json();
+      //setFiles(data.data);
+   }
+
    return (
       <DataScroller/>
    );
