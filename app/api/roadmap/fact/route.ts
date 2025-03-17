5c1629f3a3ef1072f101123f475897e0b002158c
Pavlinovich68 <pavlinovich@list.ru>
2025-03-17 16:00:38 +0500

    1

--- app/api/roadmap/fact/route.ts
+++ app/api/roadmap/fact/route.ts
@@ -1,33 +1,42 @@
 import prisma from "@/prisma/client";
+import DateHelper from "@/services/date.helpers";
 import {NextRequest, NextResponse} from "next/server";
 
 export const POST = async (request: NextRequest) => {
    try {
       const params: { year: number, month: number, day: number, user_id: number } = await request.json();
-      const date = new Date(params.year, params.month -1, params.day)
+      const date = DateHelper.toUTC(new Date(params.year, params.month-1, params.day));
+      console.log(`!!! ${date} !!!`);
       
       const user = await prisma.users.findUnique({where: {id: params.user_id}});
       if (!user)
          throw Error("Не удалось найти пользователя");
 
-      const rows = await prisma.roadmap.findMany({
+      const roadmap = await prisma.roadmap.findFirst({ where: { year: params.year } });
+      if (!roadmap)
+         throw Error(`Не удалось найти дорожную карту за ${params.year} год`);
+
+      const rows = await prisma.roadmap_fact_item.findMany({         
          include: {
-            roadmap_items: {
+            roadmap_item: {
                include: {
-                  fact_items: {
-                     where: {
-                        employee_id: user.employee_id,
-                        month: params.month,
-                        day: params.day
-                     }
-                  }
+                  project: true,
+                  roadmap: true
+               }
+            },
+            employee: {
+               include: {
+                  staff: true
                }
             }
          },
          where: {
-            year: params.year
+            employee_id: user.employee_id,
+            roadmap_item: {
+               roadmap_id: roadmap.id
+            }
          }
-      })
+      });
 
       return await NextResponse.json({status: 'success', data: rows});
    } catch (error) {
