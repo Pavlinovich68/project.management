8c9a899de86c14f011708f956ccdb8960774e52c
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2025-03-19 22:40:41 +0500

    !

--- components/projects-in-work/ItrItemInWork.tsx
+++ components/projects-in-work/ItrItemInWork.tsx
@@ -2,12 +2,14 @@
 import React, {useRef, useState, useEffect} from "react";
 import { useSession } from "next-auth/react";
 import { IRoadmapFactItem } from "@/models/IRoadmapFactItem";
+import { classNames } from "primereact/utils";
+import { Fieldset } from "primereact/fieldset";
 
 const ItrItemInWork = ({params}: {params: IRoadmapFactItem}) => {
    return (
-      <div>
+      <p>
          {params.project_name}
-      </div>
+      </p>
    );
 };
 
