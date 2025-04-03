759cb35c98ce3520ea4f551bbad2f538c303b902
Pavlinovich68 <54423661+Pavlinovich68@users.noreply.github.com>
2025-04-03 20:35:36 +0500

    !

--- app/api/calendar/department/update_cells/route.ts
+++ app/api/calendar/department/update_cells/route.ts
@@ -13,25 +13,10 @@ interface ICell {
 }
 
 export const POST = async (request: NextRequest) => {
-   const { cells: ICell[] } = await request.json();
+   const { cells } = await request.json();
    try {
       let result = null;
-      if ()
-
-      switch (inputData.operation) {
-         case CRUD.read:
-            result = await read(inputData.model as IDataSourceRequest, inputData.params);
-            break;
-         case CRUD.create:
-            result = await create(inputData.model as IModel, inputData.params);
-            break;
-         case CRUD.update:
-            result = await update(inputData.model as IModel);
-            break;
-         case CRUD.delete:
-            result = await drop(inputData.model as number);
-            break;
-      }
+      
       return await NextResponse.json({status: 'success', data: result});
    } catch (error) {
       return await NextResponse.json({status: 'error', data: (error as Error).stack });
