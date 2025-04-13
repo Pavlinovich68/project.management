6b33e7e932ba3d2cce8048bb0263b706f8d3fafb
Pavlinovich68 <pavlinovich@list.ru>
2025-04-14 03:16:04 +0500

    поломанный

--- app/(main)/workplace/department/projectcalendar/page.tsx
+++ app/(main)/workplace/department/projectcalendar/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import ItrDivisionCalendar from "@/components/ItrDivisionCalendar";
 import ItrCalendarSwitch from "@/components/ItrMonthSwitch";
+import { IBaseEntity } from "@/models/IBaseEntity";
 import { ICalendarCell } from "@/models/ICalendar";
 import DateHelper from "@/services/date.helpers";
 import RolesHelper from "@/services/roles.helper";
@@ -9,10 +10,9 @@ import { Toolbar } from "primereact/toolbar";
 import { classNames } from "primereact/utils";
 import React, { useState } from "react";
 import styles from "./styles.module.scss";
-import { DataTable } from "primereact/datatable";
-import { IBaseEntity } from "@/models/IBaseEntity";
+import { DataView } from "primereact/dataview";
 
-interface IFactWorkReport {
+interface IFactJobReport {
    month             :number,
    day               :number,
    hours             :number,
@@ -27,6 +27,20 @@ const ProjectCalendar = () => {
    const [date, setDate] = useState<Date>(new Date())
    const [selectedCell, setSelectedCell] = useState<ICalendarCell|undefined>()
 
+   const items: IFactJobReport[] = [
+      {month: 4, day: 12, hours: 2, work_type: 0, note: '', employee: {id: 1, name: 'Aкопян Левон Дмитриевич'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 3, work_type: 0, note: '', employee: {id: 1, name: 'Aкопян Левон Дмитриевич'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 3, work_type: 0, note: '', employee: {id: 1, name: 'Aкопян Левон Дмитриевич'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 2, work_type: 0, note: '', employee: {id: 2, name: 'Гажа Константин Владимирович'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 3, work_type: 0, note: '', employee: {id: 2, name: 'Гажа Константин Владимирович'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 3, work_type: 0, note: '', employee: {id: 2, name: 'Гажа Константин Владимирович'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 2, work_type: 0, note: '', employee: {id: 3, name: 'Гореньков Аркадий Юрьевич'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 3, work_type: 0, note: '', employee: {id: 3, name: 'Гореньков Аркадий Юрьевич'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 3, work_type: 0, note: '', employee: {id: 3, name: 'Гореньков Аркадий Юрьевич'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 4, work_type: 0, note: '', employee: {id: 4, name: 'Яхин Никита Артемович'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+      {month: 4, day: 12, hours: 4, work_type: 0, note: '', employee: {id: 4, name: 'Яхин Никита Артемович'}, roadmap_item: {id: 1, name: 'Работы в рамках доработок по ЦТ за 2024 год'}},
+   ]
+
 /*
 id                Int         @id @default(autoincrement())
    month             Int
@@ -40,8 +54,6 @@ id                Int         @id @default(autoincrement())
    roadmap_item      roadmap_item @relation(fields: [roadmap_item_id], references: [id])
    parent_id         Int?  // Идентификатор строки из которой 
 */   
-
-   const data =
 
    const spiner = (
       <i className="pi pi-spin pi-spinner flex align-items-center justify-content-center" style={{ fontSize: '10rem', color: '#326fd1'}}></i>
@@ -62,6 +74,43 @@ id                Int         @id @default(autoincrement())
       setSelectedCell(cell);
    }
 
+   const itemTemplate = (item: IFactWorkReport, index: number) => {
+      return (
+          <div className="col-12" key={product.id}>
+              <div className={classNames('flex flex-column xl:flex-row xl:align-items-start p-4 gap-4', { 'border-top-1 surface-border': index !== 0 })}>
+                  <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} />
+                  <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                      <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+                          <div className="text-2xl font-bold text-900">{product.name}</div>
+                          <Rating value={product.rating} readOnly cancel={false}></Rating>
+                          <div className="flex align-items-center gap-3">
+                              <span className="flex align-items-center gap-2">
+                                  <i className="pi pi-tag"></i>
+                                  <span className="font-semibold">{product.category}</span>
+                              </span>
+                              <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
+                          </div>
+                      </div>
+                      <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+                          <span className="text-2xl font-semibold">${product.price}</span>
+                          <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      );
+  };
+
+   const listTemplate = (items: Product[]) => {
+      if (!items || items.length === 0) return null;
+
+      let list = items.map((product, index) => {
+          return itemTemplate(product, index);
+      });
+
+      return <div className="grid grid-nogutter">{list}</div>;
+  };
+
    return (
       <React.Fragment>
          <div className="grid w-full">
@@ -74,9 +123,7 @@ id                Int         @id @default(autoincrement())
                         <div className={classNames(styles.projectsList)}>
                         {/* </div><div className={classNames(styles.projectsList)} hidden={!selectedCell}> */}
                         <p>Выполненные работы на дату: <h6 style={{display: "contents"}}>{DateHelper.formatDate(new Date(date.getFullYear(), date.getMonth(), selectedCell?.day))}<sup> *</sup></h6></p>                        
-                        <DataTable>
-
-                        </DataTable>
+                        <DataView value={items} listTemplate={listTemplate} />
                      </div>                     
                   </div>
                   <small style={RolesHelper.checkRoles(session.user.roles, ['developer']) ? {display: "block"} : {display: "none"}}><sup>*</sup> Для выбора кликните с удерданием клавиши Ctrl в календаре по требуемой дате</small>
